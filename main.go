c453e7fbdab0707572a8a1dc143cb076a951e4d6
Dev-raj01 <imdevilrj@gmail.com>
2024-11-08 17:43:28 +0530

    init fiber server and setup routes
    Signed-off-by: Dev-raj01 <imdevilrj@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "github.com/Dev-raj01/sortlink/database"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/Dev-raj01/sortlink/database"
+	"github.com/Dev-raj01/sortlink/handlers"
+)
 
 func main() {
+	app := fiber.New(fiber.Config{})
 	database.InitDB()
+	handlers.InitRouter(app)
+	app.Listen(":5566")
 }
